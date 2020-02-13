2478f15877a11edf8d29fcd1ee1f26a215063927
Amundson <amulevj@dunwoody.edu>
2020-02-13 10:05:04 -0600

    Comment

---
+++ FirstBat.bat
@@ -0,0 +1,15 @@
+:: Name: Levi Amundson
+:: Date: 1/22/2020
+:: Program: FirstBat.bat
+
+@echo off
+cls
+
+Echo Logged on as: %userName% at %computerName% on %date% at %time% >> %userprofile%\FirstLog.txt
+
+Echo Have a good day %userName% >> %userprofile%\FirstLog.txt
+
+Echo ******************************* >> %userprofile%\FirstLog.txt
+Echo. >> %userprofile%\FirstLog.txt
+Echo.
+Echo An entry will be added to %userprofile%\FirstLog.txt
